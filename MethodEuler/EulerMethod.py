30b458742db6c8c746686d198d8e7afbbb122f97
maicatheus <maicatheus@gmail.com>
2022-06-27 10:26:14 -0300

    (try) add implicit method

--- MethodEuler/EulerMethod.py
+++ MethodEuler/EulerMethod.py
@@ -7,7 +7,7 @@ class EulerMethod():
     def Derived(self,cte, N):
         return -cte * N
     
-    def Calculate(self,t0,tf,dt,cte):
+    def eulerExplicit(self,t0,tf,dt,cte):
         func = self.initialCondition
         t=t0
         period = list()
@@ -19,4 +19,20 @@ class EulerMethod():
             func = func + self.Derived(cte,func)*dt
             t+=dt
         
+        return period,function
+    
+    def eulerImplicit(self,t0,tf,dt,cte):
+        func = self.initialCondition
+        t=t0
+        period = list()
+        function = list()
+        function.append(func)
+        period.append(t)
+
+        while (t<=tf):
+            func = func/(1 - self.Derived(cte,func)*dt)
+            function.append(func)
+            period.append(t)
+            t+=dt
+
         return period,function
--- MethodEuler/main.py
+++ MethodEuler/main.py
@@ -3,11 +3,22 @@ import matplotlib.pyplot as plt
 
 def main():
     eulerMethod = EulerMethod(100)
+    t0=0
+    tf=20
+    dt=0.01
+    cte=1
 
-    t,F = eulerMethod.Calculate(0,20,0.05,1)
+    t,F = eulerMethod.eulerExplicit(t0,tf,dt,cte)
 
     plt.title("Decaimento Radioativo")
-    plt.plot(t,F)
+    plt.plot(t,F,label = "Explicit")
+    plt.legend()
+
+    t,F = eulerMethod.eulerImplicit(t0,tf,dt,cte)
+    plt.plot(t,F, label = "Implicit")
+    plt.legend()
+
+
     plt.show()
 
 main()
