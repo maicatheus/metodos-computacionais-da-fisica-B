9ab1d740794feb969996cf497ec97ef1e2d56aee
maicatheus <maicatheus@gmail.com>
2022-07-04 13:30:39 -0300

    population growth

--- MethodEuler/EulerMethod.py
+++ MethodEuler/EulerMethod.py
@@ -1,22 +1,81 @@
-from typing import Any
+import math
 
 class EulerMethod():
-    def __init__(self,initialCondition):
+    def __init__(self,initialCondition,t0,tf,dt):
         self.initialCondition = initialCondition
+        self.t0 = t0
+        self.tf= tf
+        self.dt=dt
+
+    def eulerAnalytic(self,cte,k,steps):
+        from math import exp
+
+        initial = self.initialCondition
+        
+        t = self.t0
+        
+        c = (k - initial)/(initial*k)
+
+        function = list()
+        period = list()
+        count = 0
+
+
+        while(count <= steps):
+            function.append(k/(1 + c*k*exp(-cte*t)))
+            period.append(t)
+            t += self.dt
+            count += 1
+
+        return period,function
+
+
+    def derived(self,cte,N,k):
+        return cte * N*(1 - N/k)
     
-    def Derived(self,cte, N):
-        return -cte * N
-    
-    def Calculate(self,t0,tf,dt,cte):
+    def eulerExplicit(self,cte,K,steps):
         func = self.initialCondition
-        t=t0
+        t = self.t0
+
         period = list()
         function = list()
-
-        while (t<=tf):
+        count=0
+        while (count <= steps):
             function.append(func)
             period.append(t)
-            func = func + self.Derived(cte,func)*dt
-            t+=dt
+            func = func + self.derived(cte,func,K)*self.dt
+            t+=self.dt
+            count+=1
         
         return period,function
+    
+    def eulerImplicit(self,cte,steps):
+        func = self.initialCondition
+        t=self.t0
+        period = list()
+        function = list()
+        function.append(func)
+        
+        func_aux = self.initialCondition
+        period.append(t)
+        
+        while (t <= self.tf):
+            func = func_aux/(1 + cte*self.dt)
+            func_aux = func
+            function.append(func)
+            t += self.dt
+            period.append(t)
+
+        return period,function       
+
+    def Error(db1,db2,k):
+        error_list = list()
+        if(len(db1) == len(db2)):
+            steps=(len(db1))
+            for i in range(0,steps):
+                err = abs(db1[i] - db2[i])/db2[i]
+                error_list.append(err)
+
+            return error_list
+        else:
+            print("list are not same length")
--- MethodEuler/main.py
+++ MethodEuler/main.py
@@ -2,16 +2,50 @@ from EulerMethod import EulerMethod
 import matplotlib.pyplot as plt 
 
 def main():
-    eulerMethod = EulerMethod(100)
+    initial_condition = 10
+    t0=0
+    tf=1E3
+    steps = 1E4
+    cte=0.1
+    K = 1E4
+    dt = 0.1
 
-    t,F = eulerMethod.Calculate(0,20,0.05,1)
+    eulerMethod = EulerMethod(initial_condition,t0,tf,dt)
+    
+    t_exp,F_exp = eulerMethod.eulerExplicit(cte,K,steps)
+    
+    t_anal, F_anal = eulerMethod.eulerAnalytic(cte,K,steps)
 
-    plt.title("Decaimento Radioativo")
-    plt.plot(t,F)
+    plt.figure(1)
+    plt.title("Populaiton growth")
+    plt.plot(t_exp,F_exp,label = f"Explicit dt={dt}")
+    plt.legend()
+
+    plt.plot(t_anal,F_anal,label = f"Analytic dt={dt}")
+    plt.legend()
+       
+    
+    
+    error_list = list()
+    dt_list = list()
+    for index in range(1,11):
+        dt_list.append(dt)
+    
+        eulerMethod = EulerMethod(initial_condition,t0,tf,dt)
+        
+        t_exp,F_exp = eulerMethod.eulerExplicit(cte,K,steps)
+        t_anal, F_anal = eulerMethod.eulerAnalytic(cte,K,steps)
+        err = EulerMethod.Error(F_exp,F_anal,K)
+        error_list.append(max(err))
+        
+        dt = dt*2
+
+    
+    plt.figure(2)
+    plt.title("Error")
+    plt.plot(dt_list,error_list,label = f"Error")
+    plt.legend()
+
     plt.show()
 
 main()
-
-
-    
-
